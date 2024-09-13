7f189d31b1ba417ae83efab7a73b7512779c8cd1
OEOTYAN <OEOTYAN@outlook.com>
2024-09-14 00:12:11 +0800

    chore: use install tool

--- packages/o/otight_pair/xmake.lua
+++ packages/o/otight_pair/xmake.lua
@@ -3,8 +3,8 @@ package("otight_pair")
     set_description("otight_pair")
 
     add_urls("https://github.com/OEOTYAN/tight_pair.git")
-    add_versions("v0.1.0", "b682ea0728f7a9fc6052c340a252d1c8586bb51e")
+    add_versions("v0.1.0", "c069414b93499b3bdf82a91e38b4df4b70f69da0")
 
     on_install(function (package)
-        os.cp("include/*", package:installdir("include"))
+        import("package.tools.xmake").install(package)
     end)
