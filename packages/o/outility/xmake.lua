5d57842243883fe557b68c41fe91aad065f55e57
OEOTYAN <OEOTYAN@outlook.com>
2024-09-15 01:42:12 +0800

    repo(outility): update version

--- packages/o/outility/xmake.lua
+++ packages/o/outility/xmake.lua
@@ -3,7 +3,7 @@ package("outility")
     set_description("oeotyan's utility")
 
     add_urls("https://github.com/OEOTYAN/utility.git")
-    add_versions("v0.1.0", "c4eacf151c08e36375d63ba2de80a955a7a5c22f")
+    add_versions("v0.1.1", "7879182608782159c93fb378a9dffaa3a1b4a7bf")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
